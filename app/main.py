1fa26da5ad0cddffa88b1dc357e9ba406b427077
Radek M <46869083+crushyna@users.noreply.github.com>
2021-04-18 13:55:53 +0200

    cognitivemodel.py added

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 import os
 from datetime import datetime
 from typing import Optional
-
+from .models.cognitivemodel import CognitiveModel
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import cv2
-from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -18,8 +17,16 @@ SNAPSHOTS_FOLDER = 'app/static/snapshots'
 camera = cv2.VideoCapture(0)
 
 
-class Session(BaseModel):
-    status: Optional[str]
+class Session:
+    def __init__(self, status: Optional[str] = None, snapshot: Optional[str] = None):
+        self.status = status
+        self.snapshot = snapshot
+        self.snapshot_fullpath = None
+
+    def clear(self):
+        self.status = None
+        self.snapshot = None
+        self.snapshot_fullpath = None
 
 
 session = Session()
@@ -49,6 +56,7 @@ async def homepage(request: Request, status='start',
                    nav_2='nav-link',
                    tab_1='tab-pane active',
                    tab_2='tab-pane'):
+    session.clear()
     return templates.TemplateResponse("index.html", {"request": request, "status": status,
                                                      "nav_1": nav_1,
                                                      "nav_2": nav_2,
@@ -64,11 +72,22 @@ async def get_snapshot(request: Request, status='snapshot',
                        tab_2='tab-pane active'):
     current_timestamp: str = datetime.now().strftime('%d%m%y%H%M%S')
     snapshot_filename = f'snapshot_{current_timestamp}.jpg'
+    session.snapshot = snapshot_filename
+    session.snapshot_fullpath = os.path.join(SNAPSHOTS_FOLDER, session.snapshot)
     success, frame = camera.read()
-    cv2.imwrite(os.path.join(SNAPSHOTS_FOLDER, snapshot_filename), frame)
+    cv2.imwrite(session.snapshot_fullpath, frame)
     return templates.TemplateResponse("index.html", {"request": request, "status": status,
                                                      "nav_1": nav_1,
                                                      "nav_2": nav_2,
                                                      "tab_1": tab_1,
                                                      "tab_2": tab_2,
-                                                     "snapshot": snapshot_filename})
+                                                     "snapshot": session.snapshot})
+
+
+@app.post('/process', response_class=HTMLResponse)
+async def process_file(request: Request, status='snapshot'):
+    cognitive_model = CognitiveModel(session.snapshot)
+    cognitive_model.get_image_desc()
+    return templates.TemplateResponse("results.html", {"request": request, "status": status,
+                                                       "result_img": session.snapshot,
+                                                       "result_desc": cognitive_model.description})
---
+++ app/models/cognitivemodel.py
@@ -0,0 +1,34 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
+
+class CognitiveModel:
+    def __init__(self, image_file: str):
+        self.subscription_key = "a3747513ff724c229b41274483acb06f"
+        self.endpoint = "https://cvision-crushyna.cognitiveservices.azure.com/"
+        self.computervision_client = ComputerVisionClient(self.endpoint,
+                                                          CognitiveServicesCredentials(self.subscription_key))
+        self.image_file = image_file
+        self.description = None
+
+    def get_image_desc(self) -> str:
+        """
+        Describe an Image - remote
+        This example describes the contents of an image with the confidence score.
+        """
+        # print("===== Describe an image - remote =====")
+        # Call API
+        description_results = self.computervision_client.describe_image(self.image_file)
+
+        # Get the captions (descriptions) from the response, with confidence level
+        if len(description_results.captions) == 0:
+            no_desc = "No description detected."
+            self.description = no_desc
+            return no_desc
+        else:
+            for caption in description_results.captions:
+                desc_found = "'{}' with confidence {:.2f}%".format(caption.text, caption.confidence * 100)
+                self.description = desc_found
+                return desc_found
