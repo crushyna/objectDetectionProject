c68e39ef22ecaeb99aad5a22f32f84e4f13d681c
Radek M <46869083+crushyna@users.noreply.github.com>
2021-04-19 20:53:59 +0200

    simple logging added

--- app/models/cognitivemodel.py
+++ app/models/cognitivemodel.py
@@ -1,3 +1,5 @@
+import logging
+
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
@@ -20,16 +22,21 @@ class CognitiveModel:
         """
         # print("===== Describe an image - remote =====")
         # Call API
+        logging.info("Opening image file as binary file")
         local_image = open(self.image_file, 'rb')
+
+        logging.info("Streaming data to Azure Server")
         description_results = self.computervision_client.describe_image_in_stream(local_image)
 
         # Get the captions (descriptions) from the response, with confidence level
         if len(description_results.captions) == 0:
             no_desc = "No description detected."
+            logging.warning(no_desc)
             self.description = no_desc
             return no_desc
         else:
             for caption in description_results.captions:
+                logging.info(f"Description found: {caption.text}")
                 desc_found = "'{}' with confidence {:.2f}%".format(caption.text, caption.confidence * 100)
                 self.description = desc_found
                 return desc_found
